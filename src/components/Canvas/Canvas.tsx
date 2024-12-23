6c0f82dabe796909e80ab51afc42785bf18c7795
Tobias Maestrini <tobias@bee365.ch>
2024-12-23 17:10:27 +0100

    refactor Canvas component to use Question subcomponent for better code organization and readability

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,10 +1,9 @@
 import { FluentProvider, Card, CardHeader, Text, webDarkTheme } from '@fluentui/react-components';
 import React from 'react';
 import { AppContext } from '../../context/AppContext';
-import { CheckmarkCircleFilled } from '@fluentui/react-icons';
-import { tokens } from '@fluentui/react-components';
 
 import { NavigationButtons } from './NavigationButtons';
+import { Question } from '../Question/Question';
 
 export const Canvas: React.FC = () => {
   const context = React.useContext(AppContext);
@@ -13,10 +12,6 @@ export const Canvas: React.FC = () => {
 
   function isCorrectAnswer(answer: string): boolean {
     return currentQuestion?.correctAnswer === answer
-  }
-
-  const CorrectAnswerIcon = (): JSX.Element => {
-    return <CheckmarkCircleFilled fontSize={100} style={{ verticalAlign: 'baseline' }} color={tokens.colorPaletteLightGreenForeground1} />
   }
 
   return (
@@ -36,37 +31,13 @@ export const Canvas: React.FC = () => {
 
           <div className='questionBlock'>
             {currentQuestion.answers.map((answer, index) => (
-              <Card
-                style={{
-                  width: '100%',
-                  minHeight: '150px',
-                  backgroundColor: 'rgba(255, 255, 255, 0.1)',
-                  backdropFilter: 'blur(10px)',
-                  border: '1px solid rgba(255, 255, 255, 0.2)',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center'
-                }}
-                key={`${currentQuestionIndex}-${index}`}
-                orientation='vertical'
-                className={correctAnswer && isCorrectAnswer(answer) ? 'blinkingCard' : 'card-animate'}>
-                <CardHeader
-                  header={
-                    <div style={{ width: '100%', textAlign: 'center' }}>
-                      <Text weight="bold" size={1000}>{index + 1}</Text>
-                    </div>
-                  }
-                />
-                <div className='question'>
-                  <div style={{ margin: '10px auto' }}>
-                    <Text weight="semibold" size={800}>
-                      {answer ?? `Antwort ${index + 1} nicht vorhanden`}
-                    </Text>
-                  </div>
-                  {correctAnswer && isCorrectAnswer(answer) && <CorrectAnswerIcon />}
-                </div>
-              </Card>
+              <Question
+                currentQuestionIndex={currentQuestionIndex}
+                index={index}
+                answer={answer}
+                correctAnswer={correctAnswer}
+                isCorrectAnswer={isCorrectAnswer(answer)}
+                key={`${currentQuestionIndex}-${index}`} />
             ))}
           </div>
 
---
+++ src/components/Question/Question.tsx
@@ -0,0 +1,52 @@
+import { Card, CardHeader, Text } from "@fluentui/react-components"
+import { CheckmarkCircleFilled } from '@fluentui/react-icons';
+import { tokens } from '@fluentui/react-components';
+
+
+interface QuestionProps {
+  currentQuestionIndex: number;
+  index: number;
+  answer: string;
+  correctAnswer?: boolean;
+  isCorrectAnswer: boolean;
+}
+
+export const Question: React.FC<QuestionProps> = (props) => {
+  const { currentQuestionIndex, index, answer, correctAnswer, isCorrectAnswer } = props;
+
+  const CorrectAnswerIcon = (): JSX.Element => {
+    return <CheckmarkCircleFilled fontSize={100} style={{ verticalAlign: 'baseline' }} color={tokens.colorPaletteLightGreenForeground1} />
+  }
+
+  return (<Card
+    style={{
+      width: '100%',
+      minHeight: '150px',
+      backgroundColor: 'rgba(255, 255, 255, 0.1)',
+      backdropFilter: 'blur(10px)',
+      border: '1px solid rgba(255, 255, 255, 0.2)',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}
+    key={`${currentQuestionIndex}-${index}`}
+    orientation='vertical'
+    className={correctAnswer && isCorrectAnswer ? 'blinkingCard' : 'card-animate'}>
+    <CardHeader
+      header={
+        <div style={{ width: '100%', textAlign: 'center' }}>
+          <Text weight="bold" size={1000}>{index + 1}</Text>
+        </div>
+      }
+    />
+    <div className='question'>
+      <div style={{ margin: '10px auto' }}>
+        <Text weight="semibold" size={800}>
+          {answer ?? `Antwort ${index + 1} nicht vorhanden`}
+        </Text>
+      </div>
+      {correctAnswer && isCorrectAnswer && <CorrectAnswerIcon />}
+    </div>
+  </Card>)
+}
